cd7c9ef5162d5386d9217b297a89094e49368a9f
DerekSteinke <derek.steinke@gmail.com>
2019-04-06 20:45:23 -0400

    Created DES_CTR decode file, not working properly

--- ctr_decode.cpp
+++ ctr_decode.cpp
@@ -5,8 +5,11 @@
     Project 1
     4-15-19
 */
+
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 #include"crypto++/cryptlib.h"
 #include"crypto++/hex.h"
@@ -16,8 +19,68 @@
 
 using namespace CryptoPP;
 
-int main()
+std::string decode(std::string & cipherText, byte counter[], byte key[])
+{
+    std::string plainText; 
+
+    try
+    {
+        CTR_Mode< DES >::Decryption decrypt;
+        decrypt.SetKeyWithIV( key, DES::DEFAULT_KEYLENGTH, counter );
+        StringSource(cipherText, true, new StreamTransformationFilter(decrypt, new StringSink(plainText)));
+
+    }
+    catch( CryptoPP::Exception& e )
+    {
+        std::cerr << e.what() << std::endl;
+        exit(1);
+    }
+
+    return plainText;
+
+}
+
+int main(int argc, char * argv[])
 {
     std::cout << "Lets begin decoding >:)" << std::endl;
+    //buffer for reading file stream
+    std::stringstream stream;
+
+    // streams for each file
+    std::fstream inputFile;
+    std::fstream outputFile;
+
+    std::string cipherText;
+
+    // for now, i am hard coding test case values
+    byte test_key[8] = {0x06, 0x1B, 0xDA, 0x66, 0xB6, 0x74, 0x7E, 0x15};
+    byte test_counter[8] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6};
+
+    // check command line args
+    if(argc != 3)
+    {
+        std::cout << "directions: ./ctr_decode <infile> <outfile>" << std::endl;
+        exit(0);
+    }
+
+    // open up stream to files
+    inputFile.open(argv[1], std::ios::in);
+    outputFile.open(argv[2], std::ios::out);
+
+    // extract input file data into plaintext string
+    stream << inputFile.rdbuf();
+    cipherText = stream.str();
+
+    std::cout << "Incoming Ciphertext: " << cipherText << std::endl;
+
+    std::string plainText = decode(plainText, test_counter, test_key);
+
+    std::cout << "Resulting Plaintext: " << plainText << std::endl;
+
+    outputFile << plainText;
+
+    inputFile.close();
+    outputFile.close();
+
     return 0;
 }
