16c385e96b873e96fa119daec46539b1f70c6cb1
DerekSteinke <derek.steinke@gmail.com>
2019-04-11 15:55:48 +0000

    Update cbc_decode.cpp to include manual padding. Sort of works but not really

--- cbc_decode.cpp
+++ cbc_decode.cpp
@@ -17,32 +17,45 @@
 #include"crypto++/des.h"
 #include "crypto++/modes.h"
 
+#define BLOCK_SIZE 8
+
 using namespace CryptoPP;
 
-std::string decode(std::string & cipherText, byte counter[], byte key[])
+//decrytion algormithm before XORing with the last block
+void des_decryption_8(unsigned char *input, unsigned char *key, unsigned char *output)
 {
-    std::string plainText; 
+	DESDecryption desDecryptor;
+	unsigned char xorBlock[BLOCK_SIZE];
+	memset(xorBlock, 0, BLOCK_SIZE);
+	desDecryptor.SetKey(key, BLOCK_SIZE);
+	desDecryptor.ProcessAndXorBlock(input, xorBlock, output);	
+}
 
-    try
+// unpack incoming ciphertext into char array
+unsigned char * getData(std::string input)
+{
+    unsigned char * data = new unsigned char[input.length()];
+
+    // map each character from string to the array
+    for(int i = 0; i < input.length(); i++)
     {
-        CBC_Mode< DES >::Decryption decrypt;
-        decrypt.SetKeyWithIV( key, DES::DEFAULT_KEYLENGTH, counter );
-        StringSource(cipherText, true, new StreamTransformationFilter(decrypt, new StringSink(plainText)));
-
-    }
-    catch( CryptoPP::Exception& e )
-    {
-        std::cerr << e.what() << std::endl;
-        exit(1);
+        data[i] = input[i];
     }
 
-    return plainText;
+    return data;
+}
 
+// obtain value of padding and update size of data
+void removePadding(unsigned char * data, int * size)
+{
+    // obtain last byte to obtain padding value
+    unsigned char paddingValue = data[*size - 1];
+
+    *size = *size - paddingValue;
 }
 
 int main(int argc, char * argv[])
 {
-    std::cout << "Lets begin decoding >:)" << std::endl;
     //buffer for reading file stream
     std::stringstream stream;
 
@@ -52,10 +65,9 @@ int main(int argc, char * argv[])
 
     std::string cipherText;
 
-    // for now, i am hard coding test case values
-    byte test_key[8] = {0x06, 0x1B, 0xDA, 0x66, 0xB6, 0x74, 0x7E, 0x15};
-    byte test_counter[8] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6};
-
+    unsigned char key[BLOCK_SIZE] = {0x06, 0x1B, 0xDA, 0x66, 0xB6, 0x74, 0x7E, 0x15};
+    unsigned char iVector[BLOCK_SIZE] = {0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6};
+	unsigned char tempBlock[BLOCK_SIZE] = {0x00,0x00,0x00,0x00, 0x00, 0x00, 0x00, 0x00};
     // check command line args
     if(argc != 3)
     {
@@ -73,9 +85,44 @@ int main(int argc, char * argv[])
 
     std::cout << "Incoming Ciphertext: " << cipherText << std::endl;
 
-    std::string plainText = decode(cipherText, test_counter, test_key);
+    // unpack incoming ciphertext into a char array
+    int dataSize = cipherText.length();
+    unsigned char * cipherData = getData(cipherText);
+    unsigned char * plainData = new unsigned char[dataSize];
+    
+    
+    // process each CBC block into plaintext, for every 8 byte chunk
+    for(int i = 0; i < dataSize; i += BLOCK_SIZE)
+    {
+        unsigned char output[BLOCK_SIZE];
+        
+        for(int j = 0; j < BLOCK_SIZE; j++)
+        {
+        	tempBlock[j] = cipherData[i + j];
+        }
 
-    std::cout << "Resulting Plaintext: " << plainText << std::endl;
+        // get result from DES of key and cipherData...
+        des_decryption_8(tempBlock, key, output);
+
+         // xor result with ciphertext block or initialization vector to get plaintext block
+        for(int j = 0; j < BLOCK_SIZE; j++)
+        {
+        	if(i == 0) plainData[i +j] = iVector[j] ^ output[j];
+            else plainData[i + j] = cipherData[i + j - BLOCK_SIZE] ^ output[j];
+        }
+
+    }
+
+    // detect padding and update size
+    removePadding(plainData, &dataSize);
+
+    std::string plainText;
+    for(int i = 0; i < dataSize; i++)
+    {
+        plainText += plainData[i];
+    }
+
+    std::cout << "\n\nResulting Plaintext: " << plainText << std::endl;
 
     outputFile << plainText;
 
